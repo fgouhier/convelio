4859fb2f1120544f42aa9779ab72634727af2d40
Florian Gouhier <florian@homeexchange.com>
2021-04-20 00:11:56 +0200

    Add docblock in Formatter.php

--- src/Service/Formatter.php
+++ src/Service/Formatter.php
@@ -2,11 +2,21 @@
 
 class Formatter
 {
+    /**
+     * @param Quote $quote
+     *
+     * @return string
+     */
     public static function renderHtml(Quote $quote)
     {
         return '<p>' . $quote->id . '</p>';
     }
 
+    /**
+     * @param Quote $quote
+     *
+     * @return string
+     */
     public static function renderText(Quote $quote)
     {
         return (string) $quote->id;
